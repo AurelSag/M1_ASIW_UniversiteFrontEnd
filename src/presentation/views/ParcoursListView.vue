a2e43ac7db3fb39bd13bc2240ed20f30cdaaafae
aurelien <aurelien.saguet7@gmail.com>
2025-12-01 14:19:36 +0100

    exercice 4

---
+++ src/presentation/views/ParcoursListView.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+import { BootstrapButtonEnum } from '@/types/BootstrapButtonEnum';
+
+import CustomButton from '@/presentation/components/forms/components/CustomButton.vue';
+
+import ParcoursForm from '@/presentation/components/forms/ParcoursForm.vue';
+
+import CustomTable from '../components/tables/CustomTable.vue';
+
+import { Parcours } from '@/domain/entities/Parcours';
+
+import { ParcoursDAO } from '@/domain/daos/ParcoursDAO';
+import parcoursForm from '@/presentation/components/forms/ParcoursForm.vue';
+
+const parcours = ref<Parcours[]>([]);
+
+const formatterEdition = (parcours: Parcours) => {
+
+  return '<i class="bi bi-pen-fill text-primary"></i>';
+
+};
+
+const formatterSuppression = (parcours: Parcours) => {
+
+  return '<i class="bi bi-trash-fill text-danger"></i>';
+
+};
+
+const columns = [
+
+  { field: 'EditionParcours', label: 'Edition', formatter: formatterEdition, onClick: () => { },
+    style:
+      "white-space: nowrap;" +
+      "width: auto;" +
+      "display: flex;" +
+      "align-items: center;" +
+      "justify-content: center;"
+  },
+
+  { field: 'ID', label: 'ID', formatter: null },
+
+  { field: 'NomParcours', label: 'Intitulé', formatter: null, onClick: null },
+
+  { field: 'AnneeFormation', label: 'Année', formatter: null, onClick: null },
+
+  { field: 'DeleteParcours', label: 'Suppression', formatter: formatterSuppression, onClick: () => { } },
+
+];
+
+onMounted(() => {
+
+  ParcoursDAO.getInstance().list().then((data) => {
+
+    parcours.value = data;
+
+  });
+
+});
+</script>
+
+<template>
+
+  <div class="container-fluid">
+
+    <div class="card mt-5">
+
+      <div class="card-header">
+
+        <div class="card-title">
+
+          <h4>Liste des parcours</h4>
+
+        </div>
+
+        <CustomButton :color="BootstrapButtonEnum.info" @click="() => parcoursForm?.openForm()">
+
+          Ajouter un parcours
+
+        </CustomButton>
+
+      </div>
+
+      <div class="card-body">
+
+        <CustomTable idAttribute="ID" :columns="columns" :data="parcours" />
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <ParcoursForm ref="parcoursForm" :parcours="null" />
+
+</template>
